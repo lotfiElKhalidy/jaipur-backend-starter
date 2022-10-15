27190c95a9af8905f0970f61ce802a725fc6ffc9
loelkhalid <lotfi.elkhalidy@gmail.com>
2022-10-15 18:03:40 +0200

    Implemented the Post and Get methods and wrote functions to create a new game

---
+++ src/routes/gameRouter.js
@@ -0,0 +1,23 @@
+import express from "express"
+import * as gameService from "../services/gameService"
+import * as databaseService from "../database"
+
+const router = express.Router()
+
+// Listen to POST /games
+router.post("/", function (req, res) {
+    if (!req.body.name) {
+      return res.status(400).send("Missing name parameter")
+    }
+    const newGame = gameService.createGame(req.body.name)
+    res.status(201).json({ id: newGame.id, name: newGame.name })
+})
+
+// Get /games
+router.get("/", function (req, res) {
+    const listGame = databaseService.getGames()
+    res.status(200).json(listGame)
+})
+
+
+export default router
